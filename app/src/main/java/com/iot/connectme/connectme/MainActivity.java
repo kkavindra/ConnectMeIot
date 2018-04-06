0416d087ab381a6cf449f4e68bc93bb44700a07d
kkavindra <harsh1kav1>
2018-04-07 00:46:10 +0200

    Initial Version for MQTT modified

--- app/src/main/java/com/iot/connectme/connectme/MainActivity.java
+++ app/src/main/java/com/iot/connectme/connectme/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -19,13 +20,17 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class MainActivity extends AppCompatActivity implements MqttCallback {
 
     private static final String TAG = "MainActivity" ;
     private TextView mTextMessage;
-    private TextView mTextSndMessage,mTextRecMessage, mTextTopicSend,mTextQos,mTextMqttBroker,mTextClientId,mTextTopicReceive;
-    private Button mButtonSendCommand,mButtonRecIns;
+    private TextView mTextSndMessage,mTextRecMessage, mTextTopicSend,mTextQos,mTextMqttBroker,mTextClientId,mTextTopicReceive,mTextExecCommand,mTextExecOut;
+    private Button mButtonSendCommand,mButtonRecIns,mButtonExec;
     MqttClient mqttAppClient;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -57,15 +62,23 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
 
         //variables to accesss the view items
         mTextMessage = (TextView) findViewById(R.id.message);
-        mTextSndMessage = (TextView) findViewById(R.id.mTextSndMessage);
-        mTextRecMessage = (TextView) findViewById(R.id.mTextRecMessage);
-        mTextTopicSend =  (TextView) findViewById(R.id.mTextTopicSend);
+
+
         mTextTopicReceive =  (TextView) findViewById(R.id.mTextTopicReceive);
         mTextQos        = (TextView) findViewById(R.id.mTextQos);
         mTextMqttBroker = (TextView) findViewById(R.id.mTextMqttBroker);
         mTextClientId = (TextView) findViewById(R.id.mTextClientId);
+
+        mTextTopicSend =  (TextView) findViewById(R.id.mTextTopicSend);
         mButtonSendCommand = (Button) findViewById(R.id.mButtonSendCommand);
+        mTextSndMessage = (TextView) findViewById(R.id.mTextSndMessage);
+
         mButtonRecIns = (Button)findViewById(R.id.mButtonRecIns);
+        mTextRecMessage = (TextView) findViewById(R.id.mTextRecMessage);
+
+        mButtonExec = (Button)findViewById(R.id.mButtonExec);
+        mTextExecCommand = (TextView) findViewById(R.id.mTextExecCommand);
+        mTextExecOut = (TextView) findViewById(R.id.mTextExecOut);
 
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -75,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
             @Override
             public void onClick(View v) {
                 try {
+                    execCommand();
                     sendMessage(mTextSndMessage.getText().toString(),Integer.parseInt(mTextQos.getText().toString()), mTextTopicSend.getText().toString());
                 } catch (MqttException e) {
                     e.printStackTrace();
@@ -91,6 +105,14 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
                 } catch (MqttException e) {
                     e.printStackTrace();
                 }
+            }
+        });
+
+        mButtonExec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView)findViewById(R.id.mTextExecOut)).setText(run(mTextExecCommand.getText().toString()));
+                ;
             }
         });
 
@@ -140,16 +162,64 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
     private void receiveMessageForTopic(int qos,String topic) throws MqttException {
         if(connectToBroker(mTextMqttBroker.getText().toString())) {
             mqttAppClient.subscribe(topic,qos);
-            Toast.makeText(getApplicationContext(), "Topic Subscribed.",Toast.LENGTH_LONG ).show();
+            //Toast.makeText(getApplicationContext(), "Topic Subscribed.",Toast.LENGTH_LONG ).show();
         }
 
     }
 
+    private void execCommand()  {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            int status=0;
+             runtime.exec("date > /data/mylog.log");
+
+            Toast.makeText(getApplicationContext(), "Status " + status,Toast.LENGTH_LONG ).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Error writing to file",Toast.LENGTH_LONG ).show();
+        }
+
+    }
+
+    public String run(String cmd) {
+        if (cmd == "clear") {
+            cls();
+            return "";
+        }
+        try {
+            // Executes the command.
+            Process process = Runtime.getRuntime().exec(cmd);
+            // Reads stdout.
+            // NOTE: You can write to stdin of the command using
+            //       process.getOutputStream().
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            int read;
+            char[] buffer = new char[4096];
+            StringBuffer output = new StringBuffer();
+            while ((read = reader.read(buffer)) > 0) {
+                output.append(buffer, 0, read);
+            }
+            reader.close();
+
+            // Waits for the command to finish.
+            process.waitFor();
+
+            return output.toString();
+        } catch (Exception e) {
+            Log.e("Caught Exception", e.toString());
+            return "Error: command "+cmd + " not found!";
+        }
+    }
+
+    public void cls() {
+        ((TextView)findViewById(R.id.mTextExecOut)).setText("");
+    }
 
 
     @Override
     public void connectionLost(Throwable cause) {
-
+        Toast.makeText(getApplicationContext(), "Connection lost to MQTT broker.",Toast.LENGTH_LONG ).show();
     }
 
     @Override
@@ -157,10 +227,12 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
         System.out.println("  Topic:\t" + topic +
                 "  Message:\t" + new String(message.getPayload()) +
                 "  QoS:\t" + message.getQos());
+        mTextTopicReceive.setText("message received");
     }
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken token) {
+        Toast.makeText(getApplicationContext(), "Delivery complete.",Toast.LENGTH_LONG ).show();
 
     }
 }
